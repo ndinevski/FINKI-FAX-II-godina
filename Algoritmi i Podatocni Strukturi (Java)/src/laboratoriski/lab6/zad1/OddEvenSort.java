be513f004afffff29b8a18e7eb0925988ecf706b
ndinevski <ndinevski5@gmail.com>
2022-11-14 21:15:15 +0100

    .

---
+++ Algoritmi i Podatocni Strukturi (Java)/src/laboratoriski/lab6/zad1/OddEvenSort.java
@@ -0,0 +1,53 @@
+//Дадена е низа со N природни броеви. Треба да се сортира низата така што во првиот дел од низата ќе бидат
+// подредени непарните броеви од неа во растечки редослед, а во вториот дел парните броеви во опаѓачки редослед.
+//Во првиот ред од влезот даден е бројот на елементи во низата N, а во вториот ред се дадени броевите. На излез
+// треба да се испечати сортираната низа.
+
+
+package laboratoriski.lab6.zad1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class OddEvenSort {
+
+    static void oddEvenSort(int a[], int n)
+    {
+        int [] nova = new int[n];
+        Arrays.sort(a);
+
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(a[i]%2==1){
+                nova[k] = a[i];
+                k++;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(a[i]%2==0){
+                nova[k]=a[i];
+                k++;
+            }
+        }
+
+        for(int i=0;i<n-1;i++)
+            System.out.print(nova[i]+" ");
+        System.out.print(nova[n-1]);
+    }
+
+    public static void main(String[] args) throws IOException{
+        int i;
+        BufferedReader stdin = new BufferedReader( new InputStreamReader(System.in));
+        String s = stdin.readLine();
+        int n = Integer.parseInt(s);
+
+        s = stdin.readLine();
+        String [] pom = s.split(" ");
+        int [] a = new int[n];
+        for(i=0;i<n;i++)
+            a[i]=Integer.parseInt(pom[i]);
+        oddEvenSort(a,n);
+    }
+}
